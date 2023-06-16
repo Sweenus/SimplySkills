a1b5d0ef313f7a2a1b3a3e78f371f4e2415f7c77
Sweenus <48658188+Sweenus@users.noreply.github.com>
2023-06-16 14:22:18 +1000

    Begin implementation of json file management

--- src/main/java/net/sweenus/simplyskills/SimplySkills.java
+++ src/main/java/net/sweenus/simplyskills/SimplySkills.java
@@ -3,12 +3,17 @@ package net.sweenus.simplyskills;
 import net.fabricmc.api.ModInitializer;
 import net.puffish.skillsmod.SkillsAPI;
 import net.sweenus.simplyskills.rewards.PassiveSkillReward;
+import net.sweenus.simplyskills.util.FileGen;
 
 public class SimplySkills implements ModInitializer {
     @Override
     public void onInitialize() {
 
         PassiveSkillReward.registerSkillTypes();
+
+        //Copy files from bundled data once we figure out how do
+        //FileGen.generateSkillFiles();
+        //FileGen.checkFileDirectory();
 
     }
 }
