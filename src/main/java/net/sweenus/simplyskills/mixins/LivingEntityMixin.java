6e5c4448acb3347d313dfd5bae570bec35895381
Sweenus <48658188+Sweenus@users.noreply.github.com>
2023-07-24 00:10:33 +1000

    Crash Fix - Fixed crash relating to LivingEntityMixin when outside of IDE

--- src/main/java/net/sweenus/simplyskills/mixins/LivingEntityMixin.java
+++ src/main/java/net/sweenus/simplyskills/mixins/LivingEntityMixin.java
@@ -1,11 +1,16 @@
 package net.sweenus.simplyskills.mixins;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.network.Packet;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.World;
 import net.sweenus.simplyskills.registry.EffectRegistry;
 import net.sweenus.simplyskills.util.Abilities;
 import net.sweenus.simplyskills.util.AbilityEffects;
@@ -21,8 +26,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class LivingEntityMixin {
 
 
+
     //Prevent detection when stealthed
-    @Inject(at = @At("HEAD"), method = "canTarget*", cancellable = true)
+    @Inject(at = @At("HEAD"), method = "canTarget(Lnet/minecraft/entity/LivingEntity;)Z", cancellable = true)
     public void simplyskills$canTarget(LivingEntity target, CallbackInfoReturnable<Boolean> cir) {
         if (target.hasStatusEffect(EffectRegistry.STEALTH))
             cir.setReturnValue(false);
