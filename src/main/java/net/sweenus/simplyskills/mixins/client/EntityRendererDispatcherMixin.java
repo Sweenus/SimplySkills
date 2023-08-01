d0b68341552e5aff82c432e0598d74bdab941373
Sweenus <48658188+Sweenus@users.noreply.github.com>
2023-08-01 14:56:13 +1000

    Further Improvements to Stealth effect - Player no longer flickers when effects are reapplied in stealth - Player held items are no longer visible when they are stealthed - Player shadow no longer flickers when effects are reapplied in stealth

---
+++ src/main/java/net/sweenus/simplyskills/mixins/client/EntityRendererDispatcherMixin.java
@@ -0,0 +1,28 @@
+package net.sweenus.simplyskills.mixins.client;
+
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.EntityRenderDispatcher;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.world.WorldView;
+import net.spell_engine.api.effect.Synchronized;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = EntityRenderDispatcher.class)
+public abstract class EntityRendererDispatcherMixin {
+
+    //Prevents shadow flickering visible when effects are reapplied
+
+    @Inject(method = "renderShadow", at = @At("HEAD"), cancellable = true)
+    private static void simplyskills$renderShadow(MatrixStack matrices, VertexConsumerProvider vertexConsumers, Entity entity, float opacity, float tickDelta, WorldView world, float radius, CallbackInfo ci) {
+        if (entity instanceof LivingEntity livingEntity) {
+            if (Synchronized.effectsOf(livingEntity).toString().contains("StealthEffect")) {
+                ci.cancel();
+            }
+        }
+    }
+}
