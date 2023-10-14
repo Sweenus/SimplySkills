cc5c2294f028bcf808cde2939908beddb542a49e
Swenus <sweeney.a.james@gmail.com>
2023-10-14 21:00:44 +1100

    Balance - Reduced Spellweaver's Weapon Expert Spellforged stacks

--- src/main/java/net/sweenus/simplyskills/abilities/ClericAbilities.java
+++ src/main/java/net/sweenus/simplyskills/abilities/ClericAbilities.java
@@ -30,49 +30,7 @@ public class ClericAbilities {
     //------- SIGNATURE ABILITIES --------
 
     // Divine Intervention
-    public static boolean signatureDivineIntervention(String crusaderSkillTree, PlayerEntity player) {
-        Vec3d blockpos = null;
-        boolean success = false;
-        int heavensmithsCallRange = SimplySkills.crusaderConfig.signatureCrusaderHeavensmithsCallRange;
-        int duration = SimplySkills.crusaderConfig.signatureCrusaderHeavensmithsCallDADuration;
-
-        if (HelperMethods.getTargetedEntity(player, heavensmithsCallRange) != null)
-            blockpos = HelperMethods.getTargetedEntity(player, heavensmithsCallRange).getPos();
-
-        if (blockpos == null)
-            blockpos = HelperMethods.getPositionLookingAt(player, heavensmithsCallRange);
-
-        if (blockpos != null) {
-            int xpos = (int) blockpos.getX();
-            int ypos = (int) blockpos.getY();
-            int zpos = (int) blockpos.getZ();
-            BlockPos searchArea = new BlockPos(xpos, ypos, zpos);
-            Box box = HelperMethods.createBoxAtBlock(searchArea, 1);
-            for (Entity entities : player.getWorld().getOtherEntities(player, box, EntityPredicates.VALID_LIVING_ENTITY)) {
-                if (entities != null) {
-                    if ((entities instanceof LivingEntity le) && !HelperMethods.checkFriendlyFire(le, player)) {
-                        success = true;
-
-                        if (HelperMethods.isUnlocked(crusaderSkillTree,
-                                SkillReferencePosition.clericSpecialisationDivineInterventionMighty, player))
-                            HelperMethods.incrementStatusEffect(le, EffectRegistry.MIGHT, duration, 4, 20);
-
-                        if (HelperMethods.isUnlocked("simplyskills:crusader",
-                                SkillReferencePosition.clericSpecialisationDivineInterventionSpellforged, player))
-                            HelperMethods.incrementStatusEffect(le, EffectRegistry.SPELLFORGED, duration, 4, 20);
-
-                        le.addStatusEffect(new StatusEffectInstance(StatusEffects.GLOWING, duration));
-
-                        SignatureAbilities.castSpellEngineIndirectTarget(player,
-                                "simplyskills:physical_heavensmiths_call",
-                                3, le);
-                        break;
-                    }
-                }
-            }
-        }
-        return success;
-    }
+    //Your next healing spell cast grants the target Undying for X seconds
 
 
 }
