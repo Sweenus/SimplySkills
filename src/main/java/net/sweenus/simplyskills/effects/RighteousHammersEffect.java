037cce844796355656f10e590f01444144ef7d51
Sweenus <48658188+Sweenus@users.noreply.github.com>
2024-03-08 19:54:02 +1100

    New features - Continue implementation of Ascendancy Tree - Finalise Righteous Hammers ability

--- src/main/java/net/sweenus/simplyskills/effects/RighteousHammersEffect.java
+++ src/main/java/net/sweenus/simplyskills/effects/RighteousHammersEffect.java
@@ -1,15 +1,29 @@
 package net.sweenus.simplyskills.effects;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.predicate.entity.EntityPredicates;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
+import net.minecraft.world.World;
+import net.sweenus.simplyskills.abilities.AscendancyAbilities;
+import net.sweenus.simplyskills.abilities.SignatureAbilities;
+import net.sweenus.simplyskills.effects.instance.SimplyStatusEffectInstance;
+import net.sweenus.simplyskills.registry.EffectRegistry;
+import net.sweenus.simplyskills.registry.SoundRegistry;
 import net.sweenus.simplyskills.util.HelperMethods;
 import net.sweenus.simplyskills.util.SkillReferencePosition;
 
+import java.util.Comparator;
+import java.util.List;
 import java.util.Objects;
 
 public class RighteousHammersEffect extends StatusEffect {
@@ -38,6 +52,24 @@ public class RighteousHammersEffect extends StatusEffect {
                             le.timeUntilRegen = 0;
                         }
                     });
+
+            if (livingEntity instanceof PlayerEntity player && AscendancyAbilities.getAscendancyPoints(player) > 29) {
+                int radiusThrow = (int) (radius * 2.5);
+                Box box2 = HelperMethods.createBox(livingEntity, radiusThrow);
+                ServerWorld world = (ServerWorld) livingEntity.getWorld();
+                List<Entity> nearbyEntities = world.getOtherEntities(livingEntity, box2, EntityPredicates.VALID_LIVING_ENTITY);
+
+                if (!nearbyEntities.isEmpty()) {
+                    Entity randomEntity = nearbyEntities.get(player.getRandom().nextInt(nearbyEntities.size()));
+
+                    if (randomEntity instanceof LivingEntity ee) {
+                        if (HelperMethods.checkFriendlyFire(ee, player)) {
+                            BlockPos blockPos = ee.getBlockPos().up(1);
+                            SignatureAbilities.castSpellEngineIndirectTarget(player, "simplyskills:righteous_hammer_projectile", 20, ee, blockPos);
+                        }
+                    }
+                }
+            }
         }
         super.applyUpdateEffect(livingEntity, amplifier);
     }
--- src/main/java/net/sweenus/simplyskills/registry/ModelRegistry.java
+++ src/main/java/net/sweenus/simplyskills/registry/ModelRegistry.java
@@ -23,7 +23,8 @@ public class ModelRegistry {
                 new Identifier(MOD_ID, "projectile/comet_projectile"),
                 new Identifier(MOD_ID, "projectile/arcane_slash"),
                 new Identifier(MOD_ID, "projectile/arrow"),
-                new Identifier(MOD_ID, "projectile/righteous_shield")
+                new Identifier(MOD_ID, "projectile/righteous_shield"),
+                new Identifier(MOD_ID, "projectile/righteous_hammers")
         ));
     }
 
